555d9142bd14b3852e8edd4ab92da5378eaf8cca
KOSASIH <kosasihg88@gmail.com>
2024-03-18 01:21:47 +0700

    Create runEndToEndTests.py

---
+++ Tests/runEndToEndTests.py
@@ -0,0 +1,29 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+def runEndToEndTests():
+    # Set up the webdriver
+    driver = webdriver.Firefox()
+
+    # Navigate to the home page
+    driver.get("http://localhost:5000")
+
+    # Find the input field for the search query
+    search_input = driver.find_element_by_name("search")
+
+    # Enter a search query and press Enter
+    search_input.send_keys("test")
+    search_input.send_keys(Keys.RETURN)
+
+    # Find the first search result
+    first_result = driver.find_element_by_css_selector(".search-results li:first-child")
+
+    # Check that the first search result's text matches the expected text
+    assert first_result.text == "Something's interfering with time, Mr. Scarman, " + \
+        "and time is my business."
+
+    # Additional end-to-end tests can be added here
+
+    # Close the webdriver
+    driver.quit()
